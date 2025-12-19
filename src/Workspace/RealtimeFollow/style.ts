6b9b2a340294aeb3af042095d3afddcd0496b7d7
“shuyan” <shuyan.wwj@antgroup.com>
2025-12-19 14:43:35 +0800

    fix(styles): adjust tab margin and add font size to RealtimeFollow component
    - Updated the margin for the tabs component to use a single variable for consistency.
    - Added a font size of 24px to the RealtimeFollow component for improved readability.

--- src/Workspace/RealtimeFollow/style.ts
+++ src/Workspace/RealtimeFollow/style.ts
@@ -78,6 +78,7 @@ const genStyle: GenerateStyle<ChatTokenType> = (token) => {
           alignItems: 'center',
           justifyContent: 'center',
           borderRadius: '6px',
+          fontSize: '24px',
 
           [`&--html`]: {
             width: '40px',
